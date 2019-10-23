056c91c496b9ca82d4c0a236ae4160f6e0c47b31
Jennifer Barker <jenny.barker2@hotmail.co.uk>
2019-10-23 22:07:19 +0100

    added bottom navigation and fragments

--- app/src/main/java/com/jenny/android/wedding/MainActivity.java
+++ app/src/main/java/com/jenny/android/wedding/MainActivity.java
@@ -1,14 +1,79 @@
 package com.jenny.android.wedding;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.jenny.android.wedding.Fragments.AboutFragment;
+import com.jenny.android.wedding.Fragments.GamesFragment;
+import com.jenny.android.wedding.Fragments.HomeFragment;
+import com.jenny.android.wedding.Fragments.PhotoFragment;
+import com.jenny.android.wedding.Fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListnener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListnener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.nav_about:
+                            selectedFragment = new AboutFragment();
+                            break;
+
+                        case R.id.nav_photos:
+                            selectedFragment = new PhotoFragment();
+                            break;
+
+                        case R.id.nav_games:
+                            selectedFragment = new GamesFragment();
+                            break;
+
+                        case R.id.nav_profile:
+                            SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                            editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            editor.apply();
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+
+                    if(selectedFragment!= null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+                    }
+
+                    return true;
+
+                }
+            };
 }
