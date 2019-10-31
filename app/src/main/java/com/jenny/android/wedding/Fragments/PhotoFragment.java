336db12e01dc93e3a1eb41314cd6f63701555797
Jennifer Barker <jenny.barker2@hotmail.co.uk>
2019-10-31 11:30:32 +0000

    Completed PostActivity

--- app/src/main/java/com/jenny/android/wedding/Fragments/PhotoFragment.java
+++ app/src/main/java/com/jenny/android/wedding/Fragments/PhotoFragment.java
@@ -1,6 +1,7 @@
 package com.jenny.android.wedding.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,7 +10,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.jenny.android.wedding.PostActivity;
 import com.jenny.android.wedding.R;
 
 public class PhotoFragment extends Fragment {
@@ -18,8 +21,21 @@ public class PhotoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_photo, container, false);
+
+        ImageView add_image_button = view.findViewById(R.id.add_image);
+
+        add_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(), PostActivity.class);
+                startActivity(i);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo, container, false);
+        return view;
     }
 
 }
