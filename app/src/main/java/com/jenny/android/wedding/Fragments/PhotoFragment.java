a1ad0490f30ae40c186ed9dc49420dc78595fe0a
Jennifer Barker <jenny.barker2@hotmail.co.uk>
2019-11-01 21:33:08 +0000

    Completed newsfeed

--- app/src/main/java/com/jenny/android/wedding/Fragments/PhotoFragment.java
+++ app/src/main/java/com/jenny/android/wedding/Fragments/PhotoFragment.java
@@ -5,18 +5,34 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jenny.android.wedding.Adapters.PostAdapter;
 import com.jenny.android.wedding.PostActivity;
 import com.jenny.android.wedding.R;
+import com.jenny.android.wedding.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,8 +50,49 @@ public class PhotoFragment extends Fragment {
             }
         });
 
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postList = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(), postList);
+        recyclerView.setAdapter(postAdapter);
+
+
+        readPosts();
+
         // Inflate the layout for this fragment
         return view;
     }
+
+    private void readPosts() {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    postList.add(post);
+
+                }
+
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 
 }
