26b85ee1636590659dc9c1b4d25a7b6855eaca14
Jennifer Barker <jenny.barker2@hotmail.co.uk>
2020-01-12 10:51:22 +0000

    Notifications page

---
+++ app/src/main/java/com/jenny/android/wedding/model/Notification.java
@@ -0,0 +1,43 @@
+package com.jenny.android.wedding.model;
+
+public class Notification {
+
+    private String userid;
+    private String text;
+    private String postid;
+
+    public Notification() {
+    }
+
+    public Notification(String userid, String text, String postid) {
+        this.userid = userid;
+        this.text = text;
+        this.postid = postid;
+
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getPostid() {
+        return postid;
+    }
+
+    public void setPostid(String postid) {
+        this.postid = postid;
+    }
+
+}
